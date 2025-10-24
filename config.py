2824eded712d5a58d9324e5168c1fabf8a6da112
aritagy <aramigaray1909@gmail.com>
2025-10-24 10:16:38 -0300

    git commit -m "cambios pequeños modificados" git push

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import pygame
 
 DIMENSION_PANTALLA = (1200, 800)
 
-LOGO_MINECRAFT = pygame.image.load("parcial_pygame/imagenes/minecraft_logo.png")
+LOGO_MINECRAFT = pygame.image.load("imagenes/minecraft_logo.png")
 
-FONDO_PANTALLA = pygame.image.load("parcial_pygame/imagenes/imagen_fondo.jpg")
+FONDO_PANTALLA = pygame.image.load("imagenes/imagen_fondo.jpg")
 
 GRIS = [80, 80, 80]
