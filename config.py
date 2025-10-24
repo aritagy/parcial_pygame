7b94cb2046888f37fd030ebe4ffbb3ad8aa718fb
aritagy <aramigaray1909@gmail.com>
2025-10-24 12:54:09 +0000

    git add . git commit -m "Archivos agregados, Ara" git push

---
+++ config.py
@@ -0,0 +1,9 @@
+import pygame
+
+DIMENSION_PANTALLA = (1200, 800)
+
+LOGO_MINECRAFT = pygame.image.load("parcial/imagenes/minecraft_logo.png")
+
+FONDO_PANTALLA = pygame.image.load("2do_parcial/imagenes/imagen_fondo.jpg")
+
+GRIS = [80, 80, 80]
---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+from funciones import *
+import config
+import pygame.mixer as mixer
+
+pygame.init()
+mixer.init()
+
+# mixer.music.load("2do_parcial/sonidos/minero.mp3")
+# mixer.music.set_volume(0.1)
+# mixer.music.play()
+
+pantalla = pygame.display.set_mode(config.DIMENSION_PANTALLA)
+
+pygame.display.set_caption("Pygirls")
+pygame.display.set_icon(config.LOGO_MINECRAFT)
+
+
+color_pantalla = [0, 0, 0]
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    pantalla.fill(color_pantalla)
+
+    pantalla.blit(config.FONDO_PANTALLA, (-1400,-500))
+
+    pygame.display.flip()
