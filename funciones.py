2eae72094ef257215ddc900015c1ea3ff670df0c
aritagy <aramigaray1909@gmail.com>
2025-10-27 18:21:25 -0300

    agregado slot

--- funciones.py
+++ funciones.py
@@ -18,3 +18,25 @@ def crear_rectangulo(superficie : list, posicion : list, color: list):
     ·retorna el diccionario del rectangulo 
     '''
     return rectangulo_nuevo
+
+def crear_slots(ancho: int, alto : int, pos_x : int, pos_y : int, columna : int, fila : int, separacion : int, color : int):
+    '''
+    ### ¿Qué hace?
+    · Crea grilla de slots segun las especificaciones del diccionario
+    ### ¿Qué recibe?
+    · Ancho y alto: ancho y alto del slot; pos_x y pos_y: posicion inicial del slot; columna y fila: cantidad de filas y columnas; 
+    · separacion: espacio entre slots; color: color del slot
+    ### ¿Qué devuelve?
+    · Al usar la funcion con las especificaciones dadas, devuelve la grilla deseada
+    '''
+    lista_slots = []
+
+    for i in range(fila):
+        for j in range(columna):
+            x = pos_x + j * (ancho + separacion)
+            y = pos_y + i * (alto + separacion)
+
+            slot = crear_rectangulo([ancho, alto], [x, y], color)
+            lista_slots.append(slot)
+
+    return lista_slots
--- main.py
+++ main.py
@@ -8,17 +8,21 @@ mixer.init()
 
 mixer.music.load("sonidos/Subwoofer_Lullaby.mp3")
 mixer.music.set_volume(0.2)
-mixer.music.play()
+mixer.music.play(-1)
 
-lista_rectangulos = []
-
-lista_rectangulos.append(crear_rectangulo([500,500], [150,50], config.GRIS_CLARO))
-
-pantalla = pygame.display.set_mode(config.DIMENSION_PANTALLA)
-
-pygame.display.set_caption("Pygirls")
+pygame.display.set_caption("Pygirls - Simulador de Crafteo")
 pygame.display.set_icon(config.LOGO_MINECRAFT)
 
+
+lista_rectangulos = []
+lista_rectangulos.append(crear_rectangulo([500,500], [150,50], config.GRIS_CLARO))
+lista_rectangulos.append(crear_rectangulo([80, 80], [490, 150], config.GRIS_OSCURO))
+lista_rectangulos.append(crear_rectangulo([50, 50], [300, 150], config.GRIS_OSCURO))
+
+slots_inventario = crear_slots(50, 50, 155, 325, 9, 4, 5, config.GRIS_OSCURO)
+slot_fabricacion = crear_slots(50, 50, 250, 120, 3, 3, 5, config.GRIS_OSCURO)
+
+pantalla = pygame.display.set_mode(config.DIMENSION_PANTALLA)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -27,14 +31,17 @@ while True:
     
     pantalla.blit(config.FONDO_PANTALLA, (-1600,-490))
 
-<<<<<<< HEAD
-=======
-    pantalla.fill(config.NEGRO)
-
-    pantalla.blit(config.FONDO_PANTALLA, (-1600,-490))
->>>>>>> e37dda0d8c28fbb459531206038408f26c526316
 
     for i in range(len(lista_rectangulos)):
         pygame.draw.rect(pantalla,lista_rectangulos[i]["color"], (lista_rectangulos[i]["posicion"], lista_rectangulos[i]["superficie"]))
+
+
+    for i in slots_inventario:
+        pygame.draw.rect(pantalla,i["color"], (i["posicion"], i["superficie"]))
+        pygame.draw.rect(pantalla, config.NEGRO, (i["posicion"], i["superficie"]), 2)
     
-    pygame.display.flip()
+    for i in slot_fabricacion:
+        pygame.draw.rect(pantalla,i["color"], (i["posicion"], i["superficie"]))
+        pygame.draw.rect(pantalla, config.NEGRO, (i["posicion"], i["superficie"]), 2)
+
+    pygame.display.flip() 
