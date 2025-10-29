5c82843ef8757b7992329cae8a63df1be9f418ab
aritagy <aramigaray1909@gmail.com>
2025-10-29 19:08:59 -0300

    .

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ pygame.init()
 mixer.init()
 pygame.font.init()
 
+pantalla = pygame.display.set_mode(config.DIMENSION_PANTALLA)
 
 minecraft_font = pygame.font.Font("fuentes/Minecraft.ttf", 27)
 arial_font = pygame.font.SysFont("Arial", 55)
-arial_font = arial_font.render("→", True, (0, 0, 0))
+arial_flecha = arial_font.render("→", True, (0, 0, 0))
 texto_fabricación = minecraft_font.render("Fabricacion", True, (0, 0, 0)) 
 texto_inventario = minecraft_font.render("Inventario", True, (0, 0, 0)) 
 
@@ -21,18 +22,18 @@ mixer.music.play(-1)
 pygame.display.set_caption("Pygirls - Simulador de Crafteo")
 pygame.display.set_icon(config.LOGO_MINECRAFT)
 
-
+#Visuales
 lista_rectangulos = []
 lista_rectangulos.append(crear_rectangulo([550,500], [170,50], config.GRIS_CLARO))
 lista_rectangulos.append(crear_rectangulo([80, 80], [580, 150], config.GRIS_OSCURO))
-lista_rectangulos.append(crear_rectangulo([60, 60], [210, 160], config.GRIS_OSCURO))
 
+#Slots
 slots_inventario = crear_slots(50, 50, 200, 320, 3, 9, 5, config.GRIS_OSCURO)
 slots_acceso_rapido = crear_slots(50, 50, 200, 490, 1, 9, 5, config.GRIS_OSCURO)
 slot_fabricacion = crear_slots(50, 50, 300, 110, 3, 3, 5, config.GRIS_OSCURO)
 
-
-pantalla = pygame.display.set_mode(config.DIMENSION_PANTALLA)
+#Botones
+lista_rectangulos.append(crear_rectangulo([60, 60], [210, 160], config.GRIS_OSCURO))
 
 while True:
     for event in pygame.event.get():
@@ -62,6 +63,6 @@ while True:
 
     pantalla.blit(texto_fabricación, (300, 80))
     pantalla.blit(texto_inventario, (200, 290))
-    pantalla.blit(arial_font, (490, 150))
+    pantalla.blit(arial_flecha, (490, 150))
 
     pygame.display.flip() 
